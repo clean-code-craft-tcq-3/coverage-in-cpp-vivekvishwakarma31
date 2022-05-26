0825b261ecf9c98a431699294ac5b2bd44eefd46
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-05-26 16:59:24 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,4 +5,63 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(20, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(30, 20, 30) == NORMAL);
+}
+
+TEST_CASE("gets breach values based on cooling type")
+{
+  //Get HIGH and LOW breach values.
+  std::pair<int, int> breachValues = getTemperatureBreachValues(PASSIVE_COOLING);
+  REQUIRE(breachValues.first == 0);
+  REQUIRE(breachValues.second == 35);
+  breachValues = getTemperatureBreachValues(HI_ACTIVE_COOLING);
+  REQUIRE(breachValues.first == 0);
+  REQUIRE(breachValues.second == 45);
+  breachValues = getTemperatureBreachValues(MED_ACTIVE_COOLING);
+  REQUIRE(breachValues.first == 0);
+  REQUIRE(breachValues.second == 40);
+}
+
+TEST_CASE("classifies Temperature Breach") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,5) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,40) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,-1) == TOO_LOW);
+  
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,10) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,50) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,-1) == TOO_LOW);
+  
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,20) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,45) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,-1) == TOO_LOW);
+}
+
+TEST_CASE("Check and Alert based on cooling type)
+{
+  REQUIRE(checkAndAlert(TO_CONTROLLER,PASSIVE_COOLING,5) ==  ALERT_NOT_SENT);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,PASSIVE_COOLING,40) == ALERT_HIGH_TEMPERATURE);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,PASSIVE_COOLING,-1) == ALERT_LOW_TEMPERATURE);
+  
+  REQUIRE(checkAndAlert(TO_CONTROLLER,HI_ACTIVE_COOLING,10) ==  ALERT_NOT_SENT);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,HI_ACTIVE_COOLING,50) == ALERT_HIGH_TEMPERATURE);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,HI_ACTIVE_COOLING,-1) == ALERT_LOW_TEMPERATURE);
+  
+  REQUIRE(checkAndAlert(TO_CONTROLLER,MED_ACTIVE_COOLING,20) ==  ALERT_NOT_SENT);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,MED_ACTIVE_COOLING,45) == ALERT_HIGH_TEMPERATURE);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,MED_ACTIVE_COOLING,-1) == ALERT_LOW_TEMPERATURE);
+  
+  REQUIRE(checkAndAlert(TO_EMAIL,PASSIVE_COOLING,5) ==  ALERT_NOT_SENT);
+  REQUIRE(checkAndAlert(TO_EMAIL,PASSIVE_COOLING,40) == ALERT_HIGH_TEMPERATURE);
+  REQUIRE(checkAndAlert(TO_EMAIL,PASSIVE_COOLING,-1) == ALERT_LOW_TEMPERATURE);
+  
+  REQUIRE(checkAndAlert(TO_EMAIL,HI_ACTIVE_COOLING,10) ==  ALERT_NOT_SENT);
+  REQUIRE(checkAndAlert(TO_EMAIL,HI_ACTIVE_COOLING,50) == ALERT_HIGH_TEMPERATURE);
+  REQUIRE(checkAndAlert(TO_EMAIL,HI_ACTIVE_COOLING,-1) == ALERT_LOW_TEMPERATURE);
+  
+  REQUIRE(checkAndAlert(TO_EMAIL,MED_ACTIVE_COOLING,20) ==  ALERT_NOT_SENT);
+  REQUIRE(checkAndAlert(TO_EMAIL,MED_ACTIVE_COOLING,45) == ALERT_HIGH_TEMPERATURE);
+  REQUIRE(checkAndAlert(TO_EMAIL,MED_ACTIVE_COOLING,-1) == ALERT_LOW_TEMPERATURE);
 }
