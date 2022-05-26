dcd08a6a957d9ab732e55f0e0f205d7d4dc100f0
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-05-26 12:34:35 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,6 +1,6 @@
 #include "typewise-alert.h"
 #include <stdio.h>
-
+using namespace std;
  map<CoolingType, pair<int, int>> limit = { { CoolingType::PASSIVE_COOLING, { 0, 35 } },
 { CoolingType::HI_ACTIVE_COOLING, { 0, 45 } },
 { CoolingType::MED_ACTIVE_COOLING, { 0, 40 } }
